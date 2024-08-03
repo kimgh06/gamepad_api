8a15eddc50bc472e34a2971995c211bc39379c9f
kimgh06 <kkh061101@naver.com>
2024-08-03 21:43:42 +0900

    :console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react"
 
 export default function App() {
   const [pad, setPad] = useState<Gamepad>();
+  const [buttonLog, setButtonLog] = useState<string[]>([]);
+  const [axisLog, setAxisLog] = useState<string[]>([]);
   function Loop() {
     let pad = navigator.getGamepads()[0] as Gamepad;
     if (!pad) {
@@ -13,12 +15,14 @@ export default function App() {
       pad.buttons.forEach((button, index) => {
         if (button.pressed) {
           // console.log(`${pad.timestamp / 1000}s Button ${index} pressed on value ${button.value}`);
+          setButtonLog([...buttonLog, `${pad.timestamp / 1000}s Button ${index} pressed on value ${button.value}`])
         }
       });
 
       pad.axes.forEach((axis, index) => {
         if (Math.abs(axis) > 0.1) {
           console.log(`${pad.timestamp / 1000}s Axis ${index} on gamepad  value: ${axis}`);
+          setAxisLog([...axisLog, `${pad.timestamp / 1000}s Axis ${index} on gamepad  value: ${axis}`]);
         }
       });
     }
@@ -38,6 +42,14 @@ export default function App() {
   return <main>
     {pad && <div>
       {`Gamepad connected at index ${pad.index}: ${pad.id}. It has ${pad.buttons.length} buttons and ${pad.axes.length} axes.`}
+      <h2>Button Log</h2>
+      <ul>
+        {buttonLog.map((log, index) => <li key={index}>{log}</li>)}
+      </ul>
+      <h2>Axis Log</h2>
+      <ul>
+        {axisLog.map((log, index) => <li key={index}>{log}</li>)}
+      </ul>
     </div>}
   </main>
 }
